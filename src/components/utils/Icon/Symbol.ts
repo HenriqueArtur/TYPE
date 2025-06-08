958fb77c7d85030b9938269a42a72634e8f6c29d
Henrique Artur <contato@henriqueartur.com>
2025-06-08 12:35:27 -0300

    🚧 `HierarchyPanel`

--- src/components/utils/Icon/Symbol.ts
+++ src/components/utils/Icon/Symbol.ts
@@ -5,6 +5,7 @@ export const ICON_MAP = {
   folder: "folder",
   folder_open: "folder_open",
   game_obejct: "deployed_code",
+  game_obejct_group: "category",
   chevron_down: "keyboard_arrow_down",
   chevron_right: "keyboard_arrow_right",
   search: "search",
