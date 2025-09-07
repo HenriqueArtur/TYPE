0f510a471e82359c7a37db00dbc8db9ea5c72bcf
Henrique Artur <contato@henriqueartur.com>
2025-09-07 16:11:27 -0300

    🚚 SystemEngine -> System

--- src/__Engine__/Engines/index.ts
+++ src/__Engine__/Engines/index.ts
@@ -3,5 +3,5 @@ export { EventEngine } from "./Event";
 export { PhysicsEngine } from "./Physics";
 export { RenderEngine } from "./Render";
 export { Scene } from "./Scene";
-export { SystemEngine } from "./SystemEngine";
+export { SystemEngine } from "./System";
 export { TimeEngine } from "./Time";
